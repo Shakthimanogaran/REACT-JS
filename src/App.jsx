b51cf228e8ae1d6ff044d215d91883b7698989b6
Shakthimanogaran <sakthimanoharan2006@gmail.com>
2025-12-23 10:56:50 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DataDisplay from'./components/Dashboard.jsx'
+import Dashboard from './components/Dashboard.jsx'
+import Counter from './components/counter.jsx'
 
 function App() {
+  const[name,setName]=useState("Shakhti")
+  const[occ,setOcc]=useState("Developer")
+  function nameChange(){
+      setName("Sunil")
+      console.log(name)
+  }
   return(
     <>
-      <h1>welcome to react js</h1>
-      <h2>hello everyone</h2>
+      <Dashboard name={name} occ={occ} nameChange={nameChange}/>
+      <Counter/>
     </>
     
     
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import {StrictMode}from "react"
+import App from "../App"
+import Card from "./card"
+
+function Dashboard({name,occ,nameChange}){
+    return(
+        <Card fullName={name} occ={occ} nameChange={nameChange}/>
+    )
+}
+
+export default Dashboard
